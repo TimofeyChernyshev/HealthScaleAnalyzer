b9c30bf01b737b630c0ef893f8992cf7f275ff5d
unknown <timkachern@mail.ru>
2026-01-11 14:52:11 +0300

    feat: add Person entity

---
+++ internal/domain/person.go
@@ -0,0 +1,12 @@
+package domain
+
+import "time"
+
+type Person struct {
+	Name          string
+	BirthDate     time.Time
+	Weight        float64
+	Height        float64
+	HealthGroup   string
+	PhysicalGroup string
+}
