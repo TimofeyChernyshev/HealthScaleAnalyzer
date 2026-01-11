e7737000ef4e2e32a0340bc0b5d1e40f47f63c49
unknown <timkachern@mail.ru>
2026-01-11 17:12:46 +0300

    feat: implement embeded bmi config files for male and female

---
+++ internal/infrastructure/bmi_config/config_loader.go
@@ -0,0 +1,42 @@
+package bmiconfig
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+
+	"github.com/TimofeyChernyshev/HealthScaleAnalyzer/internal/domain"
+)
+
+//go:embed *.json
+var configFS embed.FS
+
+type BMIConfigLoader struct{}
+
+func NewBMIConfigLoader() BMIConfigLoader {
+	return BMIConfigLoader{}
+}
+
+// LoadBMIConfig загружает конфиг из embed файлов
+func (l BMIConfigLoader) LoadBMIConfig() (configMale, configFemale *domain.BMIConfig, err error) {
+	maleFile := "bmi_male.json"
+	femaleFile := "bmi_female.json"
+
+	data, err := configFS.ReadFile(maleFile)
+	if err != nil {
+		return nil, nil, fmt.Errorf("не удалось загрузить конфиг %s: %w", maleFile, err)
+	}
+	if err := json.Unmarshal(data, configMale); err != nil {
+		return nil, nil, fmt.Errorf("ошибка парсинга конфига %s: %w", maleFile, err)
+	}
+
+	data, err = configFS.ReadFile(femaleFile)
+	if err != nil {
+		return nil, nil, fmt.Errorf("не удалось загрузить конфиг %s: %w", femaleFile, err)
+	}
+	if err := json.Unmarshal(data, configFemale); err != nil {
+		return nil, nil, fmt.Errorf("ошибка парсинга конфига %s: %w", femaleFile, err)
+	}
+
+	return configMale, configFemale, nil
+}
